1068bbd1eee6cb80c2883788b5f6917af1211e1e
embulldogs99 <embulldogs99@yahoo.com>
2019-03-10 00:42:52 -0800

    Updates

--- js/goldpricez.js
+++ js/goldpricez.js
@@ -12,16 +12,28 @@ var goldchange=(newusgold-oldusgold)/oldusgold;
 var oldspxtogold=response.T1[0].spx_gold;
 var newspxtogold=response.T[0].spx_gold;
 var spxtogoldchange=(newspxtogold-oldspxtogold)/oldspxtogold;
+if (goldchange>0){
+  var goldchangeimage='<img src="/pics/green_arrow.png" height="50" width="50"/>';
+}else if (goldchangeimage<0){
+  goldchangeimage='<img src="/pics/red_arrow.png" height="50" width="50"/>';
+}else {
+  goldchangeimage='<img src="/pics/nochange.png" height="50" width="50"/>';
+}
+
+if (spxtogoldchange>0){
+  spxgoldchangeimage='<img src="/pics/green_arrow.png" height="50" width="50"/>';
+} else if (spxtogoldchange<0){
+  spxgoldchangeimage='<img src="/pics/red_arrow.png" height="50" width="50"/>';
+} else{
+  spxgoldchangeimage='<img src="/pics/nochange.png" height="50" width="50"/>';
+}
 var output='';
 output+='<tr>'+
-'<td class="goldimage"></td>'+
-'<td>'+newusgold+'</td>'+
-'<td>Gold Price Change:'+'</td>'+
-'<td>'+goldchange+'</td>'+
+'<a href="/research/gold"><td class="goldimage"></a><br><font style="font-weight:900;"$'+newusgold+'</font></td>'+
+'<td>'+goldchangeimage+goldchange+'%</td>'+
 '<td>SPX to Gold Ratio</td>'+
 '<td>'+newspxtogold+'</td>'+
-'<td>SPX to Gold Ratio Change %:</td>'+
-'<td>'+spxtogoldchange+'</td>'+
+'<td>'+spxgoldchangeimage+spxtogoldchange+'%</td>'+
 '</tr>';
 
     document.getElementById('goldtable').innerHTML=output;
