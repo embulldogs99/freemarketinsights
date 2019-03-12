07aa94204052ba244b385de19caf86e484811153
embulldogs99 <embulldogs99@yahoo.com>
2019-03-11 22:23:37 -0700

    Updates

--- js/goldpricez.js
+++ js/goldpricez.js
@@ -5,36 +5,39 @@ client.open("Get","/json/goldpricez.json",true);
 client.send();
 client.onreadystatechange=function(){
 var response=JSON.parse(client.responseText);
-var oldusgold=response.T1[0].ounce_price_usd;
-var newusgold=response.T[0].ounce_price_usd;
+var oldusgold=response.T1.ounce_price_usd;
+var newusgold=response.T.ounce_price_usd;
 var goldchange=parseFloat(((newusgold-oldusgold)/oldusgold*100).toFixed(3));
-var oldspxtogold=response.T1[0].spx_gold;
-var newspxtogold=response.T[0].spx_gold;
+var oldspxtogold=response.T1.spx_gold;
+var newspxtogold=response.T.spx_gold;
 var spxtogoldchange=parseFloat(((newspxtogold-oldspxtogold)/oldspxtogold*100).toFixed(3));
+var goldchangeimage='';
 if (goldchange>0){
-  var goldchangeimage='<div class="green_arrow"></div>';
+  goldchangeimage+='<div class="green_arrow"></div>';
 }else if (goldchangeimage<0){
-  goldchangeimage='<div class="red_arrow"></div>';
+  goldchangeimage+='<div class="red_arrow"></div>';
 }else {
-  goldchangeimage='<div class="nochange"></div>';
+  goldchangeimage+='<div class="nochange"></div>';
 }
 
+var spxgoldchangeimage='';
 if (spxtogoldchange>0){
-  spxgoldchangeimage='<div class="green_arrow"></div>';
+  spxgoldchangeimage+='<div class="green_arrow"></div>';
 } else if (spxtogoldchange<0){
-  spxgoldchangeimage='<div class="red_arrow"></div>';
+  spxgoldchangeimage+='<div class="red_arrow"></div>';
 } else{
-  spxgoldchangeimage='<div class="nochange"></div>';
+  spxgoldchangeimage+='<div class="nochange"></div>';
 }
 var output='';
 output+='<tr>'+
 '<td><a href="/research/gold"><td class="goldimage"></td></a>'+
-'<td style="font-weight:900;">$'+newusgold+'</td>'+
-'<td>'+goldchangeimage+goldchange+'%</td>'+
+'<td>$'+newusgold+'</td>'+
+'<td>'goldchange+'%</td>'+
+'<td>'+goldchangeimage+'</td>'+
 '<td>SPX/G</td>'+
 '<td>'+newspxtogold+'</td>'+
-'<td>'+spxgoldchangeimage+'</td>'+
 '<td>'+spxtogoldchange+'%</td>'+
+'<td>'+spxgoldchangeimage+'</td>'+
 '</tr>';
 
     document.getElementById('goldtable').innerHTML=output;
