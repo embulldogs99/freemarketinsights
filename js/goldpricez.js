281b24ad394350cfde79f55f5b4bec4e821cb21a
embulldogs99 <embulldogs99@yahoo.com>
2019-03-11 22:00:52 -0700

    Updates

--- js/goldpricez.js
+++ js/goldpricez.js
@@ -5,11 +5,11 @@ client.open("Get","/json/goldpricez.json",true);
 client.send();
 client.onreadystatechange=function(){
 var response=JSON.parse(client.responseText);
-var oldusgold=response.T1[0].ounce_price_usd;
-var newusgold=response.T[0].ounce_price_usd;
+var oldusgold=response.T1.ounce_price_usd;
+var newusgold=response.T.ounce_price_usd;
 var goldchange=parseFloat(((newusgold-oldusgold)/oldusgold*100).toFixed(3));
-var oldspxtogold=response.T1[0].spx_gold;
-var newspxtogold=response.T[0].spx_gold;
+var oldspxtogold=response.T1.spx_gold;
+var newspxtogold=response.T.spx_gold;
 var spxtogoldchange=parseFloat(((newspxtogold-oldspxtogold)/oldspxtogold*100).toFixed(3));
 if (goldchange>0){
   var goldchangeimage='<div class="green_arrow"></div>';
