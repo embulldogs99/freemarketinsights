b73cd038614c3ecb892fcd8d127f62e20842646e
embulldogs99 <embulldogs99@yahoo.com>
2019-03-12 00:01:49 -0700

    automated posting

--- dist/js/goldpricez.js
+++ dist/js/goldpricez.js
@@ -13,31 +13,31 @@ var newspxtogold=response.T.spx_gold;
 var spxtogoldchange=parseFloat(((newspxtogold-oldspxtogold)/oldspxtogold*100).toFixed(3));
 var goldchangeimage='';
 if (goldchange>0){
-  goldchangeimage+='<div class="green_arrow"></div>';
+  goldchangeimage+=' class="green_arrow"';
 }else if (goldchangeimage<0){
-  goldchangeimage+='<div class="red_arrow"></div>';
+  goldchangeimage+=' class="red_arrow"';
 }else {
-  goldchangeimage+='<div class="nochange"></div>';
+  goldchangeimage+=' class="nochange"';
 }
 
 var spxgoldchangeimage='';
 if (spxtogoldchange>0){
-  spxgoldchangeimage+='<div class="green_arrow"></div>';
+  spxgoldchangeimage+=' class="green_arrow"';
 } else if (spxtogoldchange<0){
-  spxgoldchangeimage+='<div class="red_arrow"></div>';
+  spxgoldchangeimage+=' class="red_arrow"';
 } else{
-  spxgoldchangeimage+='<div class="nochange"></div>';
+  spxgoldchangeimage+=' class="nochange"';
 }
 var output='';
 output+='<tr>'+
 '<td><a href="/research/gold"><td class="goldimage"></td></a>'+
 '<td>$'+newusgold+'</td>'+
 '<td>'+goldchange+'%</td>'+
-'<td>'+goldchangeimage+'</td>'+
+'<td'+goldchangeimage+'></td>'+
 '<td>SPX/G</td>'+
 '<td>'+newspxtogold+'</td>'+
 '<td>'+spxtogoldchange+'%</td>'+
-'<td>'+spxgoldchangeimage+'</td>'+
+'<td'+spxgoldchangeimage+'></td>'+
 '</tr>';
 
     document.getElementById('goldtable').innerHTML=output;
