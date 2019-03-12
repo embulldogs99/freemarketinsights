3f74dfb560e5bc343f2a01e1bed2faa1f2482e6d
embulldogs99 <embulldogs99@yahoo.com>
2019-03-11 21:34:48 -0700

    Updates

--- js/stocksorter.js
+++ js/stocksorter.js
@@ -5,7 +5,8 @@ client.open("Get","/json/stockrank.json",true);
 client.send();
 var response=JSON.parse(client.responseText);
 client.onreadystatechange=function(){
-  var output='<tr>'+
+  var output='';
+  output+='<tr>'+
   '<th>Ticker</th>'+
   '<th>Last Price</th>'+
   '<th>Target Price</th>'+
@@ -14,8 +15,8 @@ client.onreadystatechange=function(){
   '<th>Five Year Low Distance</th>'+
   '</tr>';
   for (var i=0;i<response.length;i++){
-    if (response.fiveyrlow_distance!='-'){fyld='-'}
-      else{fyld=(response.fiveyrlow_distance*100).toFixed(2)+'%'}
+    if (response.fiveyrlow_distance!='-'){fyld='-';}
+      else{fyld=(response.fiveyrlow_distance*100).toFixed(2)+'%';}
     output+='<tr>'+
     '<td>'+response.ticker+'</td>'+
     '<td>$'+response.price+'</td>'+
