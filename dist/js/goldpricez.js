80df9c8663ea623f73a543ef3d8e170ad3175ee9
embulldogs99 <embulldogs99@yahoo.com>
2019-03-12 01:15:03 -0700

    automated posting

--- dist/js/goldpricez.js
+++ dist/js/goldpricez.js
@@ -11,23 +11,20 @@ var goldchange=parseFloat(((newusgold-oldusgold)/oldusgold*100).toFixed(3));
 var oldspxtogold=response.T1[0].spx_gold;
 var newspxtogold=response.T[0].spx_gold;
 var spxtogoldchange=parseFloat(((newspxtogold-oldspxtogold)/oldspxtogold*100).toFixed(3));
+
 var goldchangeimage='';
-if (goldchange>0){
-  goldchangeimage+=' class="green_arrow"';
-}else if (goldchangeimage<0){
-  goldchangeimage+=' class="red_arrow"';
-}else {
-  goldchangeimage+=' class="nochange"';
-}
+if (goldchange>0){goldchangeimage=' class="green_arrow"';}
+if (goldchange<0){goldchangeimage=' class="red_arrow"';}
+if (goldchange==0){goldchangeimage=' class="nochange"';}
 
 var spxgoldchangeimage='';
-if (spxtogoldchange>0){spxgoldchangeimage+=' class="green_arrow"';}
-if (spxtogoldchange<0){spxgoldchangeimage+=' class="red_arrow"';}
-if (spxtogoldchange==0){spxgoldchangeimage+=' class="nochange"';}
+if (spxtogoldchange>0){spxgoldchangeimage=' class="green_arrow"';}
+if (spxtogoldchange<0){spxgoldchangeimage=' class="red_arrow"';}
+if (spxtogoldchange==0){spxgoldchangeimage=' class="nochange"';}
 
 var output='';
 output+='<tr>'+
-'<a href="/research/gold"><td class="goldimage"></td></a>'+
+'<a href="/research/gold"><td class="goldimage"><img src="/pics/gold_usd.png"/></td></a>'+
 '<td>$'+newusgold+'</td>'+
 '<td>'+goldchange+'%</td>'+
 '<td'+goldchangeimage+'></td>'+
