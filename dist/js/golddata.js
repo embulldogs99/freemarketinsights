5e761ea73966c72b70232b1d190b049c6aee2fbe
embulldogs99 <embulldogs99@yahoo.com>
2019-03-12 18:40:44 -0700

    automated posting

--- dist/js/golddata.js
+++ dist/js/golddata.js
@@ -1,12 +1,11 @@
 
-var csv = require('/js/jquery-csv.js');
+var csv = require('jquery-csv.js');
 
 var client=new XMLHttpRequest();
 client.open("Get","/csv/spxvsgoldata.csv",true);
 client.send();
 client.onreadystatechange=function(){
   var response=JSON.parse(client.responseText);
-
-var obj=$.csv.toObjects(response);
-console.log(obj);
-}
+  var obj=$.csv.toObjects(response);
+  document.getElementById('goldata').innerHTML=obj;
+};
