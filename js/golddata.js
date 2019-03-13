5c02ae114e959cd5fe8b374f30fb50de52d86bfc
embulldogs99 <embulldogs99@yahoo.com>
2019-03-12 18:29:17 -0700

    Updates

--- js/golddata.js
+++ js/golddata.js
@@ -1,4 +1,12 @@
 
-csv='/csv/spxvsgoldata.csv';
-var obj=$.csv.toObjects(csv);
+var csv = require('/js/jquery-csv.js');
+
+var client=new XMLHttpRequest();
+client.open("Get","/csv/spxvsgoldata.csv",true);
+client.send();
+client.onreadystatechange=function(){
+  var response=JSON.parse(client.responseText);
+
+var obj=$.csv.toObjects(response);
 console.log(obj);
+}
