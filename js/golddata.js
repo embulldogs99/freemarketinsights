0fac5077fd55378bea3cfa0cb86a169888130614
embulldogs99 <embulldogs99@yahoo.com>
2019-03-12 18:58:59 -0700

    Updates

--- js/golddata.js
+++ js/golddata.js
@@ -1,9 +1,21 @@
 
 
-var client=new XMLHttpRequest();
-client.open("Get","/csv/spxvsgoldata.csv",true);
-client.send();
-client.onreadystatechange=function(){
-  var obj=Papa.parse(client.responseText);
-  document.getElementById('goldata').innerHTML=obj;
-};
+
+// jQuery
+$.getScript('/js/papaparse.js', function()
+{
+
+
+
+
+  var client=new XMLHttpRequest();
+  client.open("Get","/csv/spxvsgoldata.csv",true);
+  client.send();
+  client.onreadystatechange=function(){
+    var obj=Papa.parse(client.responseText);
+    document.getElementById('goldata').innerHTML=obj;
+  };
+
+
+
+});
