11db064537b2e20a094610126e4cf1c66fb41570
embulldogs99 <embulldogs99@yahoo.com>
2019-03-14 21:07:29 -0700

    Updates

--- js/golddata.js
+++ js/golddata.js
@@ -34,6 +34,53 @@ $.getScript('/js/papaparse.js', function()
 
 
 
+      var ctx = document.getElementById('myChart').getContext('2d');
+          var myLineChart = new Chart(ctx, {
+          type: 'line',
+          data:{
+            labels:dates,
+            datasets:[{
+              label:"Gold",
+              backgroundColor: 'rgba(0,0,0,0)',
+              borderColor: 'rgb(255, 99, 132)',
+              data:gold,
+            },{
+              label:"SPX",
+              backgroundColor: 'rgba(0,0,0,0)',
+              borderColor: 'rgb(0, 99, 0)',
+              data:spx,
+            }]
+
+          },
+
+          options:{}
+      });
+
+
+
+
+            var ctx2 = document.getElementById('mycumChart').getContext('2d');
+                var myLineChart = new Chart(ctx2, {
+                type: 'line',
+                data:{
+                  labels:dates,
+                  datasets:[{
+                    label:"Gold Cumulative Return",
+                    backgroundColor: 'rgba(0,0,0,0)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data:goldcum,
+                  },{
+                    label:"SPX Cumulative Return",
+                    backgroundColor: 'rgba(0,0,0,0)',
+                    borderColor: 'rgb(0, 99, 0)',
+                    data:spxcum,
+                  }]
+
+                },
+
+                options:{}
+            });
+
 
     document.getElementById('goldata').innerHTML=dates;
   };
