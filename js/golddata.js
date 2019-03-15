ff2068e2d4632750c95ed7334535717b973ac240
embulldogs99 <embulldogs99@yahoo.com>
2019-03-14 21:02:18 -0700

    Updates

--- js/golddata.js
+++ js/golddata.js
@@ -15,11 +15,27 @@ $.getScript('/js/papaparse.js', function()
     var obj=Papa.parse(client.responseText);
 
     var output='';
+    var dates=[];
+    var gold=[];
+    var spx=[];
+    var goldcum=[];
+    var spxcum=[];
     for (var i=0;i<obj.data.length;i++){
-      output+=obj.data[i]+'<br><br>';
-    }
+        if (i!=0){
+          dates.push(obj.data[i][0]);
+          gold.push(obj.data[i][1]);
+          spx.push(obj.data[i][2]);
+          goldcum.push(obj.data[i][3]);
+          spxcum.push(obj.data[i][4]);
+        }
 
-    document.getElementById('goldata').innerHTML=output;
+      }
+
+
+
+
+
+    document.getElementById('goldata').innerHTML=dates;
   };
 
 
