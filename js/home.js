2de7575859488744c6948df24d5780097f672422
embulldogs99 <embulldogs99@yahoo.com>
2019-03-17 00:10:41 -0700

    Updates

--- js/home.js
+++ js/home.js
@@ -10,8 +10,8 @@ xhttp2.onreadystatechange=function(){
       if (response2[i].divyield==null){divyield="-";}else{divyield=response[i].divyield;}
       output2 += "<tr><div class='horizontal'>"+
       "<td><div class='newscount'>0"+(parseFloat(i)+1)+"</div></td>"+
-      '<div class="vertical"><td><a class="hometablenote" href="http://www.google.com/search?q='+response2[i].note+'"  target="_blank" >'+response2[i].note+'</a></td>'+'<br>'+
-      '<td>Last Price'+'$'+response2[i].price+
+      '<div class="vertical"><td><a class="hometablenote" href="http://www.google.com/search?q='+response2[i].note+'"  target="_blank" >'+response2[i].note+'</a>'+'<br>'+
+      'Last Price'+'$'+response2[i].price+
       'Expected Return'+response2[i].returns+'%'+
       'Dividend Yield'+divyield+
       '<a href="http://www.google.com/search?q='+response2[i].ticker+'+stock+price"  target="_blank" >'+response2[i].ticker+'</a></td>'+
