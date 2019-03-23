a82ab4fa313e0b130c3ca47a9a01ba7e41f438b4
embulldogs99 <embulldogs99@yahoo.com>
2019-03-23 11:16:09 -0700

    Updates

--- js/stocksorter.js
+++ js/stocksorter.js
@@ -11,23 +11,19 @@ client.onreadystatechange=function(){
   '<th>Last Price</th>'+
   '<th>Target Price</th>'+
   '<th>Stock Score</th>'+
-  '<th>E/P</th>'+
-  '<th>DivYield</th>'+
   '<th>Annual EPS</th>'+
-  '<th>Five Year Low %</th>'+
-  '<th>Bank</th>'+
+  '<th>Five Year Low Distance</th>'+
   '</tr>';
   for (var i=0;i<response.length;i++){
+    if (response[i].fiveyrlow_distance!='-'){fyld='-';}
+      else{fyld=(response[i].fiveyrlow_distance*100).toFixed(2)+'%';}
     output+='<tr>'+
     '<td>'+response[i].ticker+'</td>'+
     '<td>$'+response[i].price+'</td>'+
     '<td>$'+response[i].target+'</td>'+
     '<td>'+(response[i].score).toFixed(2)+'</td>'+
-    '<td>$'+(response[i].a_e_p).toFixed(2)+'</td>'+
-    '<td>'+(response[i].divyield).toFixed(2)+'%</td>'+
     '<td>$'+response[i].a_eps+'</td>'+
-    '<td>'+(response[i].fiveyrlow_percent*100).toFixed(2)+'%</td>'+
-    '<td>'+response[i].bank+'</td>'+
+    '<td>'+fyld+'</td>'+
     '</tr>';
   }
 document.getElementById('stocksort').innerHTML=output;
