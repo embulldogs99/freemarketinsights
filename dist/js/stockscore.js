e6ae87359628a5c80286ff62ce11cdaea88a4773
embulldogs99 <embulldogs99@yahoo.com>
2019-03-26 18:23:00 -0700

    automated posting

--- dist/js/stockscore.js
+++ dist/js/stockscore.js
@@ -11,13 +11,22 @@ client.onreadystatechange=function(){
   '<th>Last Price</th>'+
   '<th>Target Price</th>'+
   '<th>Stock Score</th>'+
+  '<th>DebttoEquity</th>'+
+  '<th>DivYield</th>'+
+  '<th>ROA</th>'+
+  '<th>ROE</th>'+
+  '<th>AvgEPSgrowth</th>'+
   '</tr>';
   for (var i=0;i<response.length;i++){
     output+='<tr>'+
     '<td>'+response[i].ticker+'</td>'+
     '<td>$'+response[i].price+'</td>'+
     '<td>$'+response[i].target+'</td>'+
-    '<td>'+(response[i].score).toFixed(2)+'</td>'+
+    '<td>'+(response[i].debtoequity*100).toFixed(2)+'%</td>'+
+    '<td>'+(response[i].divyield*100).toFixed(2)+'%</td>'+
+    '<td>'+(response[i].roa*100).toFixed(2)+'%</td>'+
+    '<td>'+(response[i].roe*100).toFixed(2)+'%</td>'+
+    '<td>'+(response[i].avgepsgrowthscore).toFixed(2)+'</td>'+
     '</tr>';
   }
 document.getElementById('stocksort').innerHTML=output;
