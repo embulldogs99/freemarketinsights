b10648212ddd96ea511b2318ba79fee27400beff
embulldogs99 <embulldogs99@yahoo.com>
2019-03-27 23:38:11 -0700

    automated posting

--- dist/js/portfolio.js
+++ dist/js/portfolio.js
@@ -6,7 +6,7 @@ function portfoliorun(){
   xhttp.onreadystatechange=function(){
     if (this.readyState ==4 && this.status ==200){
       var response=JSON.parse(xhttp.responseText);
-      var output='';
+      var output='<thead><tr><th>Ticker</th><th>Prices</th><th>Exp Return</th><th>Target Trend</th><th>Dividends Trend</th></tr></thead><tbody>';
       for (var i=0;i<response.length;i++){
 
         if(response[i].to_char==null){tg='-'}else{tg=response[i].to_char}
@@ -26,6 +26,7 @@ function portfoliorun(){
 
       }
 
+      output+='</tbody>'
       document.getElementById('port').innerHTML=output;
       }
     };
@@ -33,5 +34,40 @@ function portfoliorun(){
     xhttp.send();
 
 }
+
+function score(){
+var client=new XMLHttpRequest();
+client.open("Get","/json/portfoliofundamentalanalysis.json",true);
+client.send();
+client.onreadystatechange=function(){
+  var response=JSON.parse(client.responseText);
+  var output='';
+  output+='<tr>'+
+  '<th>Ticker</th>'+
+  '<th>Last Price</th>'+
+  '<th>Target Price</th>'+
+  '<th>Stock Score</th>'+
+  '<th>DebttoEquity</th>'+
+  '<th>DivYield</th>'+
+  '<th>ROA</th>'+
+  '<th>ROE</th>'+
+  '<th>AvgEPSgrowth</th>'+
+  '</tr>';
+  for (var i=0;i<response.length;i++){
+    output+='<tr>'+
+    '<td>'+response[i].ticker+'</td>'+
+    '<td>$'+response[i].price+'</td>'+
+    '<td>$'+response[i].target+'</td>'+
+    '<td>'+(response[i].debtoequity*100).toFixed(2)+'%</td>'+
+    '<td>'+(response[i].divyield*100).toFixed(2)+'%</td>'+
+    '<td>'+(response[i].roa*100).toFixed(2)+'%</td>'+
+    '<td>'+(response[i].roe*100).toFixed(2)+'%</td>'+
+    '<td>'+(response[i].avgepsgrowthscore).toFixed(2)+'</td>'+
+    '</tr>';
+  }
+document.getElementById('stockscore').innerHTML=output;
+};
+}
+score();
 
 portfoliorun();
