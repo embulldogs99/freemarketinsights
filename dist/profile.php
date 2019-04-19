c177ccd39376958f2b04cc21a85bf55220f78207
Marius Morar <marius_fey@yahoo.com>
2019-04-19 20:09:27 +0300

    Add the php login/register system

---
+++ dist/profile.php
@@ -0,0 +1,248 @@
+<?php
+// We need to use sessions, so you should always start sessions using the below code.
+session_start();
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: index.html');
+	exit();
+}
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'freemark_root';
+$DATABASE_PASS = 'freemarket01';
+$DATABASE_NAME = 'freemark_phplogindb';
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if (mysqli_connect_errno()) {
+	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+// We don't have the password or email info stored in sessions so instead we can get the results from the database.
+$stmt = $con->prepare('SELECT password, email FROM accounts WHERE id = ?');
+// In this case we can use the account ID to get the account info.
+$stmt->bind_param('s', $_SESSION['id']);
+$stmt->execute();
+$stmt->bind_result($password, $email);
+$stmt->fetch();
+$stmt->close();
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <!-- Mobile Specific Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="img/fav.png">
+    <!-- Author Meta -->
+    <meta name="Marius Morar" content="">
+    <!-- Meta Description -->
+    <meta name="description" content="">
+    <!-- Meta Keyword -->
+    <meta name="keywords" content="">
+    <!-- meta character set -->
+    <meta charset="UTF-8">
+    <!-- Site Title -->
+    <title>Free Market Insights</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,300,500,600,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    <style>
+        * {
+            box-sizing: border-box;
+            font-family: -apple-system, BlinkMacSystemFont, "segoe ui", roboto, oxygen, ubuntu, cantarell, "fira sans", "droid sans", "helvetica neue", Arial, sans-serif;
+            font-size: 16px;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+        }
+        
+        body {
+            background-color: #435165;
+        }
+        
+        .login {
+            width: 400px;
+            background-color: #ffffff;
+            box-shadow: 0 0 9px 0 rgba(0, 0, 0, 0.3);
+            margin: 100px auto;
+        }
+        
+        .login h1 {
+            text-align: center;
+            color: #5b6574;
+            font-size: 24px;
+            padding: 20px 0 20px 0;
+            border-bottom: 1px solid #dee0e4;
+        }
+        
+        .login form {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            padding-top: 20px;
+        }
+        
+        .login form label {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 50px;
+            height: 50px;
+            background-color: #3274d6;
+            color: #ffffff;
+        }
+        
+        .login form input[type="password"],
+        .login form input[type="text"] {
+            width: 310px;
+            height: 50px;
+            border: 1px solid #dee0e4;
+            margin-bottom: 20px;
+            padding: 0 15px;
+        }
+        
+        .login form input[type="submit"] {
+            width: 100%;
+            padding: 15px;
+            margin-top: 20px;
+            background-color: #3274d6;
+            border: 0;
+            cursor: pointer;
+            font-weight: bold;
+            color: #ffffff;
+            transition: background-color 0.2s;
+        }
+        
+        .login form input[type="submit"]:hover {
+            background-color: #2868c7;
+            transition: background-color 0.2s;
+        }
+    </style>
+
+    <!-- CSS
+	============================================= -->
+
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+
+    <!-- CSS - 2
+	============================================= -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5">
+
+</head>
+
+<body class="loggedin">
+
+    <!-- Preloader -->
+    <div id="loader-wrapper">
+        <div id="loader"></div>
+        <div class="loader-section section-left"></div>
+        <div class="loader-section section-right"></div>
+    </div>
+
+    <!-- Start header section -->
+    <header class="header-area" id="header-area">
+        <div class="dope-nav-container breakpoint-off">
+            <div class="container">
+                <div class="row">
+                    <!-- dope Menu -->
+                    <nav class="dope-navbar justify-content-between" id="dopeNav">
+
+                        <!-- Logo -->
+                        <a class="nav-brand" href="index.html">
+                            <img src="img/logo-long.png" width="300px" height="100px" alt="">
+                        </a>
+
+                        <!-- Navbar Toggler -->
+                        <div class="dope-navbar-toggler">
+                            <span class="navbarToggler">
+                                <span></span>
+                            <span></span>
+                            <span></span>
+                            </span>
+                        </div>
+
+                        <!-- Menu -->
+                        <div class="dope-menu">
+
+                            <!-- close btn -->
+                            <div class="dopecloseIcon">
+                                <div class="cross-wrap">
+                                    <span class="top"></span>
+                                    <span class="bottom"></span>
+                                </div>
+                            </div>
+
+                            <!-- Nav Start -->
+                            <div class="dopenav">
+                                <ul id="nav">
+                                   
+                                    <li>
+                                        <a href="profile.php">Profile</a>
+                                    </li>
+                                    <li>
+                                        <a href="logout.php">Logout</a>
+                                    </li>
+                                </ul>
+
+                            </div>
+                            <!-- Nav End -->
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Start header section -->
+
+	<section class="about-section section-gap-full relative aboutmargin" id="about-section">
+        <div class="container">
+			<h2>Profile Page</h2>
+				<p>Your account details are below:</p>
+				<table>
+					<tr>
+						<td class="font-weight-bold text-dark">Username:</td>
+						<td class="font-weight-bold text-dark"><?=$_SESSION['name']?></td>
+					</tr>
+					<tr>
+						<td class="font-weight-bold text-dark">Password:</td>
+						<td class="font-weight-bold text-dark"><?=$password?></td>
+					</tr>
+					<tr>
+						<td class="font-weight-bold text-dark">Email:</td>
+						<td class="font-weight-bold text-dark"><?=$email?></td>
+					</tr>
+                </table>
+		</div>
+    </section>
+    
+    <!-- JS
+============================================= -->
+
+    <script src="js/vendor/jquery-2.2.4.min.js"></script>
+    <script src="js/vendor/popper.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/vendor/bootstrap.min.js"></script>
+    <script src="js/jquery.parallax-scroll.js"></script>
+    <script src="js/dopeNav.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/main.js"></script>
+
+    <!-- dist  JS
+============================================= -->
+    <script src="js/Chart.js"></script>
+    <script src="js/home.js"></script>
+
+</body>
+</html>
